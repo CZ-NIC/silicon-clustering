bd88fd36d6fe1a1d9aa937918039825dbf56a741
Tomas Gavenciak <gavento@ucw.cz>
2017-01-15 20:15:44 +0100

    Add tests

---
+++ tests/test_random.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+import scipy.sparse
+import sys
+import tempfile
+import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import silicon as sc
+
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases and unit test."""
+
+    def gen_rand_data(self, N, dim, radius, density, rnd):
+        data = np.zeros((N, dim))
+        for i in range(N):
+            for j in range(dim):
+                if rnd.rand() < density:
+                    data[i][j] = (rnd.rand() - 0.5) * 2.0 * radius
+        return data
+
+    def test_random_array(self):
+        "Cluster a random 1000x100 array"
+        rnd = np.random.RandomState(42)
+        data = self.gen_rand_data(1000, 100, 1.0, 0.1, rnd)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.4, cell_dims=2, normalize=True, rnd=rnd)
+        ens.run()
+        assert (sorted(map(len, ens.clusters)))[-12:] == [4, 5, 5, 6, 7, 7, 8, 11, 11, 11, 14, 394]
+
+    def test_random_array_plain(self):
+        "Cluster a random 1000x100 array, no nibbles, dim=1"
+        rnd = np.random.RandomState(42)
+        data = self.gen_rand_data(1000, 100, 1.0, 0.1, rnd)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.4, cell_dims=1, normalize=True, rnd=rnd)
+        ens.run(nibbles=0)
+        assert (sorted(map(len, ens.clusters)))[-12:] == [4, 5, 5, 6, 7, 7, 8, 11, 11, 11, 14, 394]
+
+    def test_random_sparse(self):
+        "Cluster a random 1000x100 sparse matrix (10% full)"
+        rnd = np.random.RandomState(42)
+        data = scipy.sparse.csr_matrix(self.gen_rand_data(1000, 100, 1.0, 0.1, rnd))
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.4, cell_dims=2, normalize=True, rnd=rnd)
+        ens.run()
+        assert (sorted(map(len, ens.clusters)))[-12:] == [4, 5, 5, 6, 7, 7, 8, 11, 11, 11, 14, 394]
+
---
+++ tests/test_unit.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+import scipy.sparse
+import sys
+import tempfile
+import unittest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import silicon as sc
+
+
+class BasicTestSuite(unittest.TestCase):
+    """Basic test cases and unit test."""
+
+    TEST_ARRAY = [
+        [1.0, 1.0],
+        [1.0, 1.01],
+        [0.1, 3.0],
+        [-0.1, 3.0],
+        [-0.3, 3.0],
+        [-0.5, 3.0],
+        [-1.0, 2.0]]
+
+    def test_cell(self):
+        "Create a Cell"
+        cell = sc.cell.Cell((-2, 1))
+        assert 'no mask' in str(cell)
+
+    def test_cluster(self):
+        "Create a Cluster"
+        cluster = sc.Cluster(42)
+        assert 'Cluster no 42' in str(cluster)
+        cluster.recompute_elems(np.array([2,3,42,0,42]))
+        assert np.array_equal(cluster.elements_idx, [2, 4])
+
+    def test_ensemble_array(self):
+        "Cluster a small array, check results"
+        data = np.array(self.TEST_ARRAY)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.9942, cell_dims=2, normalize=True, rnd=np.random.RandomState(42))
+        assert 'ClusteringEnsemble [sim=0.9942' in str(ens)
+        ens.run()
+        assert np.array_equal(ens.cluster_map, [0, 0, 1, 1, 1, 1, 2])
+
+    def test_ensemble_sparse(self):
+        "Cluster a small sparse matrix, check results"
+        data = scipy.sparse.csr_matrix(self.TEST_ARRAY)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.9942, cell_dims=2, normalize=True, rnd=np.random.RandomState(42))
+        assert 'ClusteringEnsemble [sim=0.9942' in str(ens)
+        ens.run()
+        assert np.array_equal(ens.cluster_map, [0, 0, 1, 1, 1, 1, 2])
+
+    def test_plot_array(self):
+        "Cluster a small array, plot resulting clusters"
+
+        try:
+            import matplotlib as mpl
+            import matplotlib.pyplot as plt
+        except ImportError as exc:
+            self.skipTest("Matplotlib/Pyplot not found")
+
+        data = np.array(self.TEST_ARRAY)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.9942, cell_dims=2, normalize=True, rnd=np.random.RandomState(42))
+        ens.run()
+
+        plt.clf()
+        ens.plot_PCA_coords()
+
+        plt.clf()
+        ens.clusters[0].plot_cluster_location_and_zoomed(ens)
+
+    def test_plot_sparse(self):
+        "Cluster a small sparse matrix, plot resulting clusters"
+        try:
+            import matplotlib as mpl
+            import matplotlib.pyplot as plt
+        except ImportError as exc:
+            self.skipTest("Matplotlib/Pyplot not found")
+
+        data = scipy.sparse.csr_matrix(self.TEST_ARRAY)
+        ens = sc.ClusteringEnsemble(
+                data, sim_threshold=0.9942, cell_dims=2, normalize=True, rnd=np.random.RandomState(42))
+        ens.run()
+
+        plt.clf()
+        ens.plot_PCA_coords()
+
+        plt.clf()
+        ens.clusters[0].plot_cluster_location_and_zoomed(ens)
+
